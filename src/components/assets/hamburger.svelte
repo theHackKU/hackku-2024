d81047aa16ee2eeb3330c423852c2477edca4b21
zoekulphong <zoekulphong@localhost>
2024-03-27 23:29:06 +0000

    Automatically updated using GitHub Actions

--- src/components/assets/hamburger.svelte
+++
@@ -1,5 +0,0 @@
-<svg xmlns="http://www.w3.org/2000/svg" width="37" height="28" viewBox="0 0 37 28" fill="none">
-  <path fill-rule="evenodd" clip-rule="evenodd" d="M0 2C0 0.89543 0.89543 0 2 0H34.5C35.6046 0 36.5 0.89543 36.5 2C36.5 3.10457 35.6046 4 34.5 4H2C0.89543 4 0 3.10457 0 2Z" fill="#1B2526"/>
-  <path fill-rule="evenodd" clip-rule="evenodd" d="M0 14C0 12.8954 0.89543 12 2 12H34.5C35.6046 12 36.5 12.8954 36.5 14C36.5 15.1046 35.6046 16 34.5 16H2C0.89543 16 0 15.1046 0 14Z" fill="#1B2526"/>
-  <path fill-rule="evenodd" clip-rule="evenodd" d="M0 26C0 24.8954 0.89543 24 2 24H34.5C35.6046 24 36.5 24.8954 36.5 26C36.5 27.1046 35.6046 28 34.5 28H2C0.89543 28 0 27.1046 0 26Z" fill="#1B2526"/>
-</svg>
